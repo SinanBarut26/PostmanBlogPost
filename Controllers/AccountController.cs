10dc22bf718a37b084da2cec009ec29648aab6cd
Sinan <sinan_26@msn.com>
2021-04-06 15:00:38 +0300

    init

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using PostmanBlogPost.Models;
+
+namespace PostmanBlogPost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [AllowAnonymous]
+    public class AccountController : ControllerBase
+    {
+        private readonly ILogger<AccountController> _logger;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(ILogger<AccountController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        [HttpGet("Get")]
+        public IActionResult Get()
+        {
+            if (User.Claims != null && User.Claims.Any())
+                return Ok("Here you are");
+            return NotFound("User not found");
+        }
+
+        [HttpPost("Login")]
+        public IActionResult Login(LoginModel login)
+        {
+            if (login.Username != "postman")
+                return Unauthorized();
+
+            var authClaims = new List<Claim>
+                {
+                    new Claim(ClaimTypes.Name, login.Username),
+                    new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                };
+
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"]));
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddHours(3),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return Ok(new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            });
+        }
+    }
+}
---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace PostmanBlogPost.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Authorize]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("GetAll")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet("GetByName")]
+        public IActionResult Get(string name)
+        {
+            var rng = new Random();
+            var result = Summaries.FirstOrDefault(x => x.ToLower() == name);
+            if (result is null)
+                return NotFound();
+            return Ok(new WeatherForecast
+            {
+                Date = DateTime.Now,
+                Summary = result,
+                TemperatureC = rng.Next(-20, 55)
+            });
+        }
+    }
+}
